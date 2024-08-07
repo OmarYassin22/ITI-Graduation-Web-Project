28d5033bc1d21fe5fed7605168a90b9a025a2f7e
Your Name <mostafaahmed1101997@gmail.com>
2024-08-07 16:52:40 +0300

    Add Courses Page

--- app/Navbar/page.jsx
+++ app/Navbar/page.jsx
@@ -10,6 +10,7 @@ function Navbar() {
       <ul className='flex gap-x-3'>
         <li><Link href="/">Home</Link></li>
         <li><Link href="/user/About">About</Link></li>
+        <li><Link href="/user/Courses">Courses</Link></li>
         <li><Link href="/user/Contact">Contact Us</Link></li>
         <li><Link href="/signup">Sign Up</Link></li>
         <li><Link href="/login">Login</Link></li>
