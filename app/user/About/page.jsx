4e56abe41a5fb680b3d362b1d975b7c2783312e0
Amooor <amtm.gado1261998@gmail.com>
2024-08-09 12:46:54 +0300

    Amr Gado update

--- app/user/About/page.jsx
+++ app/user/About/page.jsx
@@ -3,7 +3,6 @@ import React, { useState } from 'react'
 import Image from 'next/image';
 import Contact from '../Contact/page';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
-
 //AIzaSyB6jJbqKnU4pvZoP3SntNIN5FJjmudTw34
 
 function About() {
@@ -54,9 +53,9 @@ function About() {
         Our mission is simple: to democratize education by providing affordable and flexible learning opportunities to people everywhere. Whether you're looking to develop new skills, advance in your career, or simply learn something new, we are here to support you on your journey.
       </div>
       <div className="flex flex-col justify-center items-center mt-5">
-        <label for="countries" className="block mb-2 text-xl font-medium text-gray-900 dark:text-white ">Select a branch</label>
+        <label htmlFor="countries" className="block mb-2 text-xl font-medium text-gray-900 dark:text-white ">Select a branch</label>
         <select id="countries" onChange={(e) => { handleLocation(e) }} className="bg-gray-50 border border-gray-300 text-gray-900 text-xl rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 w-96 flex justify-center items-center">
-          <option selected>Choose a branch</option>
+          <option value="choose" disabled>Choose a branch</option>
           <option value="Benha">Benha</option>
           <option value="Alex">Alex</option>
           <option value="smart">Smart Village</option>
@@ -74,7 +73,6 @@ function About() {
           </GoogleMap>
         </LoadScript>
       </div>
-
     </>
   )
 }
