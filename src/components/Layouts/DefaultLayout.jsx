e2d45887be77280a5595693d90b2a5ddd2e7a7a4
omar yassin <o.y.sleem@gmail.com>
2024-08-29 18:32:09 +0300

    fix redirect

--- src/components/Layouts/DefaultLayout.jsx
+++ src/components/Layouts/DefaultLayout.jsx
@@ -15,7 +15,7 @@ export default function DefaultLayout({ children }) {
       <>
         <div className="flex">
           <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-
+          
           <div className="relative flex flex-1 flex-col lg:ml-72.5">
             <main>
               <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
